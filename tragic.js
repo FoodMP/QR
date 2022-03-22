4a2d935f52da12062f42eba68e7fa1ba2c97e0e3
FoodMP <FoodMP@protonmail.com>
2022-03-21 23:02:11 -0400

    Create tragic.js

---
+++ tragic.js
@@ -0,0 +1,12 @@
+document.addEventListener('keydown', function() {
+  if (event.keyCode == 123) {
+    alert("smh why u tryna see my scource code");
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    alert("smh why u tryna see my scource code");
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    alert("smh why u tryna see my scource code");
+    return false;
+  }
+}, false);
