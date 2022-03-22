2885fcd8aeefa22477affde7844a3b46b31432db
FoodMP <FoodMP@protonmail.com>
2022-03-21 22:12:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const data = document.querySelector("#data");
 const qr = document.querySelector("#qr");
 
 data.addEventListener("keyup" , e =>{
-    {
+    if(e.code === "Enter"){
     qr.innerHTML = "";
     const href = data.value;
     // size of the image
