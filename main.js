9bbad9720eb30ea02c05e58909393a36996dde32
FoodMP <FoodMP@protonmail.com>
2022-03-21 22:59:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,16 +18,3 @@ data.addEventListener("keyup" , e =>{
     })
 }
 })
-
-document.addEventListener('keydown', function() {
-  if (event.keyCode == 123) {
-    alert("smh why u tryna see my scource code");
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    alert("smh why u tryna see my scource code");
-    return false;
-  } else if (event.ctrlKey && event.keyCode == 85) {
-    alert("smh why u tryna see my scource code");
-    return false;
-  }
-}, false);
